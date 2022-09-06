9f56d3811ab69f7cce1933e8850cde5aee75bb45
Joe Barr <joejbarr@gmail.com>
2022-09-06 20:59:51 +0100

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+import { largeImgWidth } from '../components/layout.module.css'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="home sweet home">
       <p>ahh. we are home.</p>
-    </Layout>
+      <StaticImage className={largeImgWidth}
+        alt='green rolling hills, blue sky with some clouds in background. windows xp wallpaper "bliss"'
+        src='../images/xp_bliss.jpg'
+        />
+     </Layout>
   )
 }
 
